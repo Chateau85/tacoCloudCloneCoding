714c24f48ea038a85bfb41607ea821c1a01e5a84
cw <cw@DESKTOP-9T5AADF>
2020-06-15 11:09:25 +0900

    TacoCloud clone coding study initial commit

---
+++ src/main/java/tacos/Chateau85TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chateau85TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chateau85TacoCloudApplication.class, args);
+	}
+
+}
